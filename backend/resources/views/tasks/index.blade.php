eff75117f9a81a4e92288bffc1c8311667af59e7
sobalimited <yamauchi.links@gmail.com>
2021-05-14 17:52:48 +0900

    タスク一覧表示

--- backend/resources/views/tasks/index.blade.php
+++ backend/resources/views/tasks/index.blade.php
@@ -17,6 +17,7 @@
   </header>
   <main>
     <div class="container">
+
       <div class="row">
         <div class="col col-md-4">
           <nav class="panel panel-default">
@@ -36,10 +37,45 @@
             </div>
           </nav>
         </div>
+
         <div class="column col-md-8">
           <!-- ここにタスクが表示される -->
+          <div class="panel panel-default">
+            <div class="panel-heading">タスク</div>
+            <div class="panel-body">
+              <div class="text-right">
+                <a href="#" class="btn btn-default btn-block">
+                  タスクを追加する
+                </a>
+              </div>
+            </div>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>タイトル</th>
+                  <th>状態</th>
+                  <th>期限</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach($tasks as $task)
+                <tr>
+                  <td>{{ $task->title }}</td>
+                  <td>
+                    <span class="label {{ $task->status_class }}">{{ $task->status_label }}</span>
+                  </td>
+                  <td>{{ $task->formatted_due_date }}</td>
+                  <td><a href="#">編集</a></td>
+                </tr>
+                @endforeach
+              </tbody>
+            </table>
+          </div>
         </div>
+
       </div>
+
     </div>
   </main>
 </body>
